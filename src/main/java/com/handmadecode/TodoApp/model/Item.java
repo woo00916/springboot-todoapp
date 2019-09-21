57d4e83d26bc92e75e6f33b95fe07ff1e74f621e
unknown <woo00916@gmail.com>
2019-09-21 14:49:59 +0900

    todoapp project start

---
+++ src/main/java/com/handmadecode/TodoApp/model/Item.java
@@ -0,0 +1,45 @@
+package com.handmadecode.TodoApp.model;
+import java.util.Date;
+import lombok.*;
+
+@AllArgsConstructor @NoArgsConstructor
+@Getter @Setter
+public class Item {
+    private String id;
+    private String title;
+    private boolean done;
+
+
+    public static class builder{
+        private String id;
+        private String title;
+        private boolean done;
+
+        public builder(){
+
+        }
+
+        public builder id(String id){
+            this.id=id;
+            return this;
+        }
+
+        public builder title(String title){
+            this.title=title;
+            return this;
+        }
+
+        public builder done(boolean done){
+            this.done=done;
+            return this;
+        }
+
+        public Item build(){
+            Item item=new Item();
+            item.id=this.id;
+            item.title=this.title;
+            item.done=this.done;
+            return item;
+        }
+    }
+}
